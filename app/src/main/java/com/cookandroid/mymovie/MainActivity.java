116f9ea425fe4cf0bf09928f05c301171ea597cd
lim nahyun <iiimna17@gmail.com>
2022-06-18 00:37:33 +0900

    라

---
+++ app/src/main/java/com/cookandroid/mymovie/MainActivity.java
@@ -0,0 +1,58 @@
+package com.cookandroid.mymovie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("영화 선호도 투표");
+
+        final int[] voteCount = new int[9];
+        for (int i = 0; i < 9; i++)
+            voteCount[i] = 0;
+
+        ImageView image[] = new ImageView[9];
+        Integer imageId[] = {R.id.iv1, R.id.iv2, R.id.iv3,
+                R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7,
+                R.id.iv8, R.id.iv9};
+        final String imgName[] = {"오징어 게임", "지옥", "스위트 홈",
+                "마이네임", "킹덤", "D.P", "인간수업", "연모", "보건교사 안은영"};
+
+        for (int i = 0; i < imageId.length; i++) {
+            final int index;
+            index = i;
+            image[index] = (ImageView) findViewById(imageId[index]);
+            image[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(),imgName[index]
+                            + ": 총" + voteCount[index] + " 표",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    }
+        Button btnFinsh = (Button) findViewById(R.id.btnResult);
+        btnFinsh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                intent.putExtra("VoteCount", voteCount);
+                intent.putExtra("ImageName", imgName);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/cookandroid/mymovie/SecondActivity.java
@@ -0,0 +1,6 @@
+package com.cookandroid.mymovie;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+}
